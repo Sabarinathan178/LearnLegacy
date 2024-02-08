cad88ee75a3249eb11da7be8f23fdf6dac3ebe35
Sudhakar0512 <sudhakar.s2021csbs@sece.ac.in>
2024-02-08 23:38:11 +0530

    first

---
+++ src/Utils/Reviews.js
@@ -0,0 +1,91 @@
+import sudhakar from "../assets/sudhakard.jpg";
+import sabari from "../assets/sabarid.jpg";
+import dhanabal from "../assets/dhanabal.jpeg";
+import vishnu from "../assets/vishnuprabhu.jpg";
+import arul from "../assets/arul.jpeg";
+
+const Reviews = [
+  {
+    id: 1,
+    name: "Sudhakar",
+    bio: "Spring & Springboot",
+    img: sudhakar,
+    description:
+      "I am an enthusiastic B.Tech student passionate about backend development, fostering a positive mindset.", //, and creating impactful solutions
+    rating: 5.0,
+  },
+  {
+    id: 2,
+    name: "Sabarinathan",
+    bio: "Front-end Developer",
+    img: sabari,
+    description:
+      "Highly motivated and passionate B.tech Student with a expertise in HTML,CSS and JavaScript and React Js and adopting to new technologies.",
+    rating: 5.0,
+  },
+  {
+    id: 3,
+    name: "Dhanabal",
+    bio: "UI/UX Designer",
+    img: dhanabal,
+    description:
+      "I'm Dhanabal, and I am a UX/UI Designer. My passion is to create a good user experience that makes people's lives easier.",
+    rating: 5.0,
+  },
+  {
+    id: 4,
+    name: "Vishnu Prabhu",
+    bio: "Artificial Intelligence",
+    img: vishnu,
+    description:
+      "Embarking on the transformative journey of technology, I am passionate about fostering understanding and inspiring learners. ",
+    rating: 5.0,
+  },
+  {
+    id: 5,
+    name: "Arul Selvan",
+    bio: "Blockchain",
+    img: arul,
+    description:
+      "Web3 Developer,Certified Blockchain Associate,Ambassador @EmergeX, Member @KBAIC, Blockchain Enthusiast. ",
+    rating: 5.0,
+  },
+  {
+    id: 6,
+    name: "kani",
+    bio: "Blockchain",
+    img: arul,
+    description:
+      "Web3 Developer,Certified Blockchain Associate,Ambassador @EmergeX, Member @KBAIC, Blockchain Enthusiast. ",
+    rating: 5.0,
+  },
+  {
+    id: 7,
+    name: "shashwin",
+    bio: "Blockchain",
+    img: arul,
+    description:
+      "Web3 Developer,Certified Blockchain Associate,Ambassador @EmergeX, Member @KBAIC, Blockchain Enthusiast. ",
+    rating: 5.0,
+  },
+  {
+    id: 8,
+    name: "shesha",
+    bio: "Blockchain",
+    img: arul,
+    description:
+      "Web3 Developer,Certified Blockchain Associate,Ambassador @EmergeX, Member @KBAIC, Blockchain Enthusiast. ",
+    rating: 5.0,
+  },
+  {
+    id: 9,
+    name: "thoufic",
+    bio: "Blockchain",
+    img: arul,
+    description:
+      "Web3 Developer,Certified Blockchain Associate,Ambassador @EmergeX, Member @KBAIC, Blockchain Enthusiast. ",
+    rating: 5.0,
+  },
+];
+
+export default Reviews;
